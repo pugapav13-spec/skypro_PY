bfdd13bc50a105c398d7d08525a2eff27d766ac7
pugapav13-spec <pugapav13@gmail.com>
2026-01-29 19:58:31 +0700

    Add files via upload

---
+++ 06K_lesson/test_02_calc.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+@pytest.mark.parametrize("delay_time, expected_res", [("45", "15")])
+def test_slow_calculator(delay_time, expected_res):
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+
+    delay_input = driver.find_element(By.ID, "delay")
+    delay_input.clear()
+    delay_input.send_keys("45")
+
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.XPATH, "//span[text()='=']").click()
+
+    wait = WebDriverWait(driver, 50)
+
+    wait.until(
+        EC.text_to_be_present_in_element((By.CLASS_NAME, "screen"), "15")
+    )
+
+    result = driver.find_element(By.CLASS_NAME, "screen").text
+    assert result == "15", f"Ошибка: калькулятор показал {result} вместо {expected_res}"
+
+    driver.quit()
